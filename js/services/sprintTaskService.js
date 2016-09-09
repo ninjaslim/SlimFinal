99cfee0ab6e034224cee40db7b30cc396936c6e3
Divyal Patel <Divyal Patel>
2016-09-09 22:15:06 +0800

    Test

--- js/services/sprintTaskService.js
+++ js/services/sprintTaskService.js
@@ -3,14 +3,10 @@
 angular.module('slim').service('sprintTaskService', function ($http) {
 
     var sprintTaskService = {
-
         loadDummyData : function () {
            return  $http.get("data/data.json");
         }
-
     };
-    
-
   return sprintTaskService;
 
 });
